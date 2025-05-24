7b66377b985361523ec737603f3d4cd2ea273293
builtbywilldev <builtbywill@proton.me>
2025-05-24 16:48:53 -0600

    working my way through structure issues one step at a time! Loving the process!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
   import Nav from "./components/Nav";
-import "./App.css"; 
 
 function App() {
   return (
     <div className="App">
       <header className="nav">
-       <Nav/>
+
+       <Nav />
+
+
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
